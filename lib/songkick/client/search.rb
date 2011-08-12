35f2b45f16dc7c62a9da73e230e674af2fb53c5b
Grégory Marcilhacy <gregorym@ciblo.net>
2011-08-12 09:34:04 +0200

    fixing search api

--- lib/songkick/client/search.rb
+++ lib/songkick/client/search.rb
@@ -3,12 +3,12 @@ module Songkick
     module Search
 
       def search_events(opts)
-        _opts = opts.collect{|k, v| "#{k}=#{v}"}.join("&")
+        _opts = opts.collect{|k, v| "#{k}=#{v.gsub(" ", "%20")}"}.join("&")
         get "events.#{format}?#{_opts}"
       end
         
       def search_location(opts)
-        _opts = opts.collect{|k, v| "#{k}=#{v}"}.join("&")
+        _opts = opts.collect{|k, v| "#{k}=#{v.gsub(" ", "%20")}"}.join("&")
         get "search/locations.#{format}?#{_opts}"
       end
 
