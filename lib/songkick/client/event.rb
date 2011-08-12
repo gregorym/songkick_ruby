b3582385321897800471d54097d05d6cdc7c80d8
Grégory Marcilhacy <gregorym@ciblo.net>
2011-08-12 09:21:23 +0200

    fixing find event

--- lib/songkick/client/event.rb
+++ lib/songkick/client/event.rb
@@ -2,12 +2,8 @@ module Songkick
   class Client
     module Event
 
-      class << self
-
-        def find_event(event_id)
-          get "/events/#{event_id}.#{format}"
-        end
-        
+      def find_event(event_id)
+        get "events/#{event_id}.#{format}"
       end
 
     end
