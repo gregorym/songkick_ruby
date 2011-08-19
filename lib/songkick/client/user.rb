b7c5ee746fb4d71041e8def7af6a2bef080fdd42
Grégory Marcilhacy <gregorym@ciblo.net>
2011-08-19 16:19:50 +0200

    Get tracked locations and artists for a user

--- lib/songkick/client/user.rb
+++ lib/songkick/client/user.rb
@@ -9,9 +9,25 @@ module Songkick
       #
       # Example:
       # sg = Songkick.new("myapikey")
-      # gigs = song.gigography("saleandro ")
+      # gigs = song.gigography("saleandro")
       def gigography(username, page = 1)
         get "users/#{username}/gigography.#{format}", page
+      end
+
+      # Get tracked locations for a user
+      # Example:
+      # sg = Songkick.new("myapikey")
+      # locations = sg.tracked_locations("saleandro")
+      def tracked_locations(username, page = 1)
+        get "users/#{username}/metro_areas/tracked.json", page
+      end
+
+      # Get tracked artists for a user
+      # Example:
+      # sg = Songkick.new("myapikey")
+      # artists = sg.tracked_artists("saleandro")
+      def tracked_artists(username, page = 1)
+        get "users/#{username}/artists/tracked.json", page
       end
 
     end
