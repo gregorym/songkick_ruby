301067cf00d6618436c141564b10544b841f5943
Grégory Marcilhacy <gregorym@ciblo.net>
2011-08-19 16:47:13 +0200

    Track an artist or a location

--- lib/songkick/client/user.rb
+++ lib/songkick/client/user.rb
@@ -19,7 +19,7 @@ module Songkick
       # sg = Songkick.new("myapikey")
       # locations = sg.tracked_locations("saleandro")
       def tracked_locations(username, page = 1)
-        get "users/#{username}/metro_areas/tracked.json", page
+        get "users/#{username}/metro_areas/tracked.#{format}", page
       end
 
       # Get tracked artists for a user
@@ -27,7 +27,17 @@ module Songkick
       # sg = Songkick.new("myapikey")
       # artists = sg.tracked_artists("saleandro")
       def tracked_artists(username, page = 1)
-        get "users/#{username}/artists/tracked.json", page
+        get "users/#{username}/artists/tracked.#{format}", page
+      end
+
+      # Track an artist
+      def track_artist(username, artist_id)
+        get "users/#{username}/trackings/artist:#{artist_id}.#{format}"
+      end
+
+      # Track a location
+      def track_location(usersame, location_id)
+        get "users/#{username}/trackings/metro_area:#{location_id}.#{format}"
       end
 
     end
