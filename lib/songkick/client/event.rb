576115a51645e05a27b3b8a7b617241e1a3602d1
Gregory Marcilhacy <greg@cruiswise.com>
2012-08-22 16:35:00 -0700

    Add event setlists API

--- lib/songkick/client/event.rb
+++ lib/songkick/client/event.rb
@@ -11,5 +11,15 @@ module Songkick
       get "events/#{event_id}.#{format}"
     end
 
+    # Returns the setlists for a specific event.
+    #
+    # Example:
+    # sg = Songkick.new("myapikey")
+    # set_list = sg.event_set_lists(123)
+    # The first argument is the event id
+    def event_set_lists(event_id)
+      get "events/#{event_id}/setlists.#{format}"
+    end
+
   end
 end
